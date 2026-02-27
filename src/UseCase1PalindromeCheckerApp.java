d379960b987a9f17ca8d3b487ee2c6dba90965a7
christonowhere <christopherwilsonjust@gmail.com>
2026-02-27 14:58:48 +0530

    UC2:Hardcoded palindrome

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -16,5 +16,34 @@
             System.out.println("==========================================");
             System.out.println("Application started successfully.");
             System.out.println("Ready for next use case implementation.");
+
+            /**
+             * Application entry point for UC2.
+             *
+             * @param args Command-line arguments
+             */
+
+            // Data Structure: String
+            String word = "madam";   // String Literal
+
+            String reversed = "";
+
+            // Reverse the string
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reversed = reversed + word.charAt(i);
+            }
+
+            // Conditional Statement (if-else)
+            if (word.equals(reversed)) {
+
+                // Console Output
+                System.out.println(word + " is a Palindrome");
+
+            } else {
+
+                // Console Output
+                System.out.println(word + " is Not a Palindrome");
+            }
         }
     }
+
